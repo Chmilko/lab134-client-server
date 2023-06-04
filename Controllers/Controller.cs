143b1f78e8bb4520f7963ef18c5f24a1d96a63e5
Ivan Kovalev <105602023+Chmilko@users.noreply.github.com>
2023-06-04 15:16:25 +0700

    Initial commit for lab123

---
+++ Controllers/Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using lab123.Models;
+
+namespace lab123.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        private static List<Data> _memCache = new List<Data>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Data>> Get()
+        {
+            return _memCache;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Data> Get(int id)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Данные отсутствуют!");
+
+            return _memCache[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Data value)
+        {
+            _memCache.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Data value)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Данные отсутствуют!");
+
+            _memCache[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Данные отсутствуют!");
+
+            _memCache.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab123.Models
+{
+    public class Data
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int Pressure { get; set; }
+        public int WindSpeed { get; set; }
+        public string Humidity { get; set; }
+    }
+}
