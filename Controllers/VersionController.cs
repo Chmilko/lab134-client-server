0b329290306e87666ff6ecdc91a8df343651ff45
Ivan Kovalev <105602023+Chmilko@users.noreply.github.com>
2023-06-04 15:22:13 +0700

    Added version control functionality

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using lab123.Models;
+
+namespace lab123.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var versionInfo = new Version
+            {
+                Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+                Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
+                ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            };
+
+            return Ok(versionInfo);
+        }
+    }
+}
